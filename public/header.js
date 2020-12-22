c81d1487075601cef54d532b2006f7d354527bbd
Kashyap Patel <khpatel4991@gmail.com>
2020-12-21 19:59:19 -0500

    Update fps freq

--- public/header.js
+++ public/header.js
@@ -1,13 +1,19 @@
 import { useLoc } from 'preact-iso/router';
 import { useEffect, useState } from 'preact/hooks';
 
+const UPDATE_FPS_INTERVAL_MS = 100;
+
 export default function Header() {
 	const { url } = useLoc();
 	const [fps, setFps] = useState(0);
 	const [lastFrame, setLastFrame] = useState(0);
+	const [lastUpdated, setLastUpdated] = useState(Number.NEGATIVE_INFINITY);
 	useEffect(() => {
 		const t = performance.now();
-		setFps(Math.round(1000 / (t - lastFrame)));
+		if(t - lastUpdated > UPDATE_FPS_INTERVAL_MS) {
+			setLastUpdated(t);
+			setFps(Math.round(1000 / (t - lastFrame)));
+		}
 		setLastFrame(t);
 	});
 	return (
